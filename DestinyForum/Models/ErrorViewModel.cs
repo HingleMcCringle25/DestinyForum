f8659524c2ab1f49a3f0ee597eabd6ade6dc765e
HingleMcCringle25 <adriandemont11@gmail.com>
2025-02-04 20:43:37 -0400

    Add project files.

---
+++ DestinyForum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DestinyForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
